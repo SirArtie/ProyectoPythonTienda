f803b4348aafc1118dae6aadebf024ba095652c9
boquer <dani_981103@hotmail.com>
2020-02-03 17:52:05 -0600

    Numero de caracteres especificados

--- proyecto2.py
+++ proyecto2.py
@@ -83,31 +83,34 @@ while(b==False):
             check_nickname2=check_nickname.isalnum() #Validando datos alfanumericos
             check_contraseña=input("Ingrese la contraseña: ")
             check_contraseña2=check_contraseña.isalnum() #validando datos alfanumericos.
-            if check_nickname in usuarios and check_nickname2 == True:#checa si el nombre ingresado por 2da vez, se encuentra en el diccionario.
-                if usuarios[nickname][0]==check_contraseña and check_contraseña2 == True:#checa si la contraseña ingresa se encuentra en la lista, posición 0 que está en el diccionario.
-                    print("Los datos son correctos, por favor de ingresar lo que se pide")
-                    nombre=input("\nNombre:")#llave del diccionario
-                    apellido=input("Apellido: ")#0
-                    edad=int(input("Edad: "))#1
-                    correo=input("Correo: ")#2
-                    tarjeta=int(input("No. Tarjeta: "))#3
-                    paypal=input("PayPal: ")#4
-                    contra_pay=input("Contraseña de PayPal: ")#5
+            if len(check_nickname) >= 6 and len(check_nickname) <=12: #Verificando el número de carácteres en el nombre de usuario.
+                if check_nickname in usuarios and check_nickname2 == True:#checa si el nombre ingresado por 2da vez, se encuentra en el diccionario.
+                    if usuarios[nickname][0]==check_contraseña and check_contraseña2 == True:#checa si la contraseña ingresa se encuentra en la lista, posición 0 que está en el diccionario.
+                        print("Los datos son correctos, por favor de ingresar lo que se pide")
+                        nombre=input("\nNombre:")#llave del diccionario
+                        apellido=input("Apellido: ")#0
+                        edad=int(input("Edad: "))#1
+                        correo=input("Correo: ")#2
+                        tarjeta=int(input("No. Tarjeta: "))#3
+                        paypal=input("PayPal: ")#4
+                        contra_pay=input("Contraseña de PayPal: ")#5
                     #Guardando datos personales en la lista
-                    lista_informacion.append(apellido)
-                    lista_informacion.append(edad)
-                    lista_informacion.append(correo)
-                    lista_informacion.append(tarjeta)
-                    lista_informacion.append(paypal)
-                    lista_informacion.append(contra_pay)
+                        lista_informacion.append(apellido)
+                        lista_informacion.append(edad)
+                        lista_informacion.append(correo)
+                        lista_informacion.append(tarjeta)
+                        lista_informacion.append(paypal)
+                        lista_informacion.append(contra_pay)
                     #Asignando la lista en la llave del diccionario.
-                    informacion[nombre]=lista
-                    a=True
+                        informacion[nombre]=lista
+                        a=True
 
+                    else:
+                        print("Contraseña incorrecta")
                 else:
-                    print("Contraseña incorrecta")
+                    print("Nombre de usuario incorrecto")
             else:
-                print("Nombre de usuario incorrecto")
+                print("El nombre de usuario debe contener un mínimo de 6 carácteres y un máximo de 12")
 
 
     if x==2:
@@ -124,24 +127,27 @@ while(b==False):
         check_contraseña=input("Ingrese la contraseña: ")
         check_contraseña2=check_contraseña.isalnum()
 
-        if check_nickname in usuarios and check_nickname2 == True:
-            if usuarios[nickname][0]==check_contraseña and check_contraseña2==True:
-                print("Bienvenido "+nickname)
-                subOpcion2 = 0
-                while subOpcion2!=4:
-                    subOpcion2 = menuR('Selecciona una opcion:\n1)Ver los Articulo\n2)Ver informacion completa de todos los articulos\n3)Comprar Articulo\n4)Cerrar Sesion',4)
-                    if subOpcion2 == 1:
-                        listarInventario()
-                    if subOpcion2 == 2:
-                        mostrarInventario()
-                    if subOpcion2 == 3:
-                        print('Selecciona alguno del numero de los siguientes articulos')
-                        listarInventario()
-                        comprar(menuR('',numeroArticulos))
+        if len(check_nickname) >= 6 and len(check_nickname) <=12:
+            if check_nickname in usuarios and check_nickname2 == True:
+                if usuarios[nickname][0]==check_contraseña and check_contraseña2==True:
+                    print("Bienvenido "+nickname)
+                    subOpcion2 = 0
+                    while subOpcion2!=4:
+                        subOpcion2 = menuR('Selecciona una opcion:\n1)Ver los Articulo\n2)Ver informacion completa de todos los articulos\n3)Comprar Articulo\n4)Cerrar Sesion',4)
+                        if subOpcion2 == 1:
+                            listarInventario()
+                            if subOpcion2 == 2:
+                                mostrarInventario()
+                                if subOpcion2 == 3:
+                                    print('Selecciona alguno del numero de los siguientes articulos')
+                                    listarInventario()
+                                    comprar(menuR('',numeroArticulos))
+                else:
+                    print("Contraseña Invalida")
             else:
-                print("Contraseña Invalida")
+                print("Usuario no registrado")
         else:
-            print("Usuario no registrado")
+            print("El nombre de usuario debe contener un mínimo de 6 carácteres y un máximo de 12")
 
 
     if x==3:
